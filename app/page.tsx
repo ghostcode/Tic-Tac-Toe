0bd59f7d98020e6ec693617ed01f4569e26348a6
zhuxy607 <zhuxy607@easthope.com>
2024-07-04 20:16:47 +0800

    edit meta title

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import TicTacToe from './tic-tac-toe'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center mt-8 mb-4">井字游戏</h1>
       <TicTacToe />
     </div>
   )
